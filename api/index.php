27d4348923a596561e89d8a52b6ef72950ce49f4
ahmadsidikrofi <rofidragon71@gmail.com>
2025-06-12 02:32:55 +0700

    update: api/index and vercel-community for build php in vercel

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
 require __DIR__ . '/../public/index.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->mke(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = \Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
